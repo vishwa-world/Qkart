fe640c9b64ac0ebe3d4ba2933dbe1d5e50acc0bd
Nabhan <nabhan@crido.com>
2022-04-20 18:40:48 +0530

    Revert "updated changes - 2022-04-20 18:20:22"
    This reverts commit 656b587d5ddf3982f1aa824fad12887d0e52d1dc.

---
+++ app/src/main/java/QKART_SANITY_LOGIN/Home.java
@@ -0,0 +1,37 @@
+package QKART_SANITY_LOGIN;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Home {
+    RemoteWebDriver driver;
+    String url = "https://crio-qkart-frontend-qa.vercel.app";
+
+    public Home(RemoteWebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigateToHome() {
+        if (!this.driver.getCurrentUrl().equals(this.url)) {
+            this.driver.get(this.url);
+        }
+    }
+
+    public Boolean PerformLogout() throws InterruptedException {
+        try {
+            // Find and click on the Logout Button
+            WebElement logout_button = driver.findElement(By.className("MuiButton-text"));
+            logout_button.click();
+
+            // Wait for Logout to Complete
+            Thread.sleep(3000);
+
+            return true;
+        } catch (Exception e) {
+            // Error while logout
+            return false;
+        }
+    }
+}
