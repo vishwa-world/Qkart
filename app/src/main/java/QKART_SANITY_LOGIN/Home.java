4827033d707aced8a01f8295dc41e2423dddb9fa
Nabhan <nabhan@criodo.com>
2022-04-23 16:29:41 +0550

    updated changes - 2022-04-23 16:09:41

--- app/src/main/java/QKART_SANITY_LOGIN/Home.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Home.java
@@ -1,5 +1,8 @@
 package QKART_SANITY_LOGIN;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,6 +34,117 @@ public class Home {
             return true;
         } catch (Exception e) {
             // Error while logout
+            return false;
+        }
+    }
+
+        /*
+     * Returns Boolean if searching for the given product name occurs without any
+     * errors
+     */
+    public Boolean searchForProduct(String product) {
+        try {
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error while searching for a product: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Returns Array of Web Elements that are search results and return the same
+     */
+    public List<WebElement> getSearchResults() {
+        List<WebElement> searchResults = new ArrayList<WebElement>() {
+        };
+        try {
+            return searchResults;
+        } catch (Exception e) {
+            System.out.println("There were no search results: " + e.getMessage());
+            return searchResults;
+
+        }
+    }
+
+    /*
+     * Returns Boolean based on if the "No products found" text is displayed
+     */
+    public Boolean isNoResultFound() {
+        Boolean status = false;
+        try {
+            return status;
+        } catch (Exception e) {
+            return status;
+        }
+    }
+
+    /*
+     * Return Boolean if add product to cart is successful
+     */
+    public Boolean addProductToCart(String productName) {
+        try {
+            /*
+             * Iterate through each product on the page to find the WebElement corresponding
+             * to the matching productName
+             * 
+             * Click on the "ADD TO CART" button for that element
+             * 
+             * Return true if these operations succeeds
+             */
+            System.out.println("Unable to find the given product");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Exception while performing add to cart: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean denoting the status of clicking on the checkout button
+     */
+    public Boolean clickCheckout() {
+        Boolean status = false;
+        try {
+            return status;
+        } catch (Exception e) {
+            System.out.println("Exception while clicking on Checkout: " + e.getMessage());
+            return status;
+        }
+    }
+
+ 
+    /*
+     * Return Boolean denoting the status of change quantity of product in cart
+     * operation
+     */
+    public Boolean changeProductQuantityinCart(String productName, int quantity) {
+        try {
+
+
+
+
+            return false;
+        } catch (Exception e) {
+            if (quantity == 0)
+                return true;
+            System.out.println(("exception occurred when updating cart"));
+            return false;
+        }
+    }
+
+   /*
+     * Return Boolean denoting if the cart contains items as expected
+     */
+    public Boolean verifyCartContents(List<String> expectedCartContents) {
+        try {
+
+
+
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Exception while verifying cart contents: " + e.getMessage());
             return false;
         }
     }
