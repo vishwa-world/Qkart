89b33a15e697fb2f9dea0835af761b12bfa09c2d
Nabhan <nabhan@criodo.com>
2022-05-11 16:54:59 +0550

    updated changes - 2022-05-11 16:34:59

--- app/src/main/java/QKART_SANITY_LOGIN/Home.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Home.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Home {
     RemoteWebDriver driver;
@@ -92,6 +93,14 @@ public class Home {
              * 
              * Return true if these operations succeeds
              */
+            // CRIO_SOLUTION_START_MODULE_TEST_AUTOMATION
+            List<WebElement> gridContent = driver.findElementsByClassName("css-sycj1h");
+            for (WebElement cell : gridContent) {
+                if (cell.findElement(By.className("css-yg30e6")).getText().equals(productName)) {
+                    cell.findElement(By.tagName("button")).click();
+                    // CRIO_SOLUTION_START_MODULE_TEST_AUTOMATION
+            // SLEEP_STMT_12: If product found, wait till the product gets added
+            // successfully
             System.out.println("Unable to find the given product");
             return false;
         } catch (Exception e) {
