8e9ec3f42a977ba5a1e880bcae2ba97284a70656
Nabhan <nabhan@criodo.com>
2022-05-11 16:54:51 +0550

    updated changes - 2022-05-11 16:34:51

--- app/src/main/java/QKART_SANITY_LOGIN/Home.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Home.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Home {
     RemoteWebDriver driver;
