c5c57f91d7ea25e561b6d508053df481a5fd7260
Nabhan <nabhan@criodo.com>
2022-05-20 21:44:09 +0550

    updated changes - 2022-05-20 21:24:09

--- selenium-setup.sh
+++ selenium-setup.sh
@@ -1,7 +1,2 @@
-# docker installation
-sudo apt-get update
-sudo apt-get -y install docker.io
-
-# pull selenium and zalenium docker images
-sudo docker pull elgalu/selenium
-sudo docker pull dosel/zalenium
+curl -O https://gitlab.crio.do/crio_bytes/selenium/me_selenium_web_actions/-/raw/master/SeleniumWebActions/selenium-run.sh
+chmod +x selenium-run.sh
