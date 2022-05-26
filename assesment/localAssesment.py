fcb57dfea474607b8d36eaa3c07f01132d4b0f8b
Nabhan <nabhan@criodo.com>
2022-05-26 23:36:59 +0550

    updated changes - 2022-05-26 23:16:59

--- assesment/localAssesment.py
+++ assesment/localAssesment.py
@@ -13,7 +13,8 @@ def construct_log_json(log_text):
     selector = None
     for line in open(log_text):
         if('[INFO]' in line and 'COMMAND' in line ):
-            LOG["actions"].append(log_object_frame)
+            if ("COMMAND ExecuteScript" not in str(log_object_frame)):
+                LOG["actions"].append(log_object_frame)
             selector = "COMMAND"
             log_object_frame={"COMMAND":None, "RESPONSE":None}
             log_frame=""
