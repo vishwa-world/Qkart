cd32e97be254f1b6b3e33e79a4431e2af26973cf
Nabhan <nabhan@criodo.com>
2022-06-28 15:05:57 +0550

    updated changes - 2022-06-28 14:45:57

--- run_Platform_Assesment.sh
+++ run_Platform_Assesment.sh
@@ -1,4 +1,7 @@
-# python3 assesment/remoteAssesment.py "0.0.0.0" assesment/AIS_MO_01.json
- cp /tmp/external_build/test-results/test/TEST-QKART_TESTNG.QKART_Tests.xml test-results.xml
+
+python3 assesment/remoteAssesment.py "0.0.0.0" assesment/AIS_MO_01.json
+# CRIO_SOLUTION_AND_STUB_START_MODULE_TESTNG
+cp /tmp/external_build/test-results/test/TEST-QKART_TESTNG.QKART_Tests.xml test-results.xml
 pip3 install -r assesment/xmlAssessment/requirements.txt
-python3 assesment/xmlAssessment/xmlAssessment.py assesment/xmlAssessment/Assessment_Instruction.json /tmp/external_build/test-results/test/TEST-QKART_TESTNG.QKART_Tests.xml
+python3 assesment/xmlAssessment/xmlAssessment.py assesment/xmlAssessment/Assessment_Instruction.jso
+# CRIO_SOLUTION_AND_STUB_END_MODULE_TESTNG
