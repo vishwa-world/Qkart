0bec9e15d6d4295278e165074da5097d670af0a0
Nabhan <nabhan@criodo.com>
2022-06-28 15:30:17 +0550

    updated changes - 2022-06-28 15:10:17

--- run_assesment.sh
+++ run_assesment.sh
@@ -6,7 +6,7 @@ then
     python3 assesment/generateFilteredLogs.py ./app/chromedriver.log
 fi
 python3 assesment/localAssesment.py ./filtered_logs.json assesment/AIS_MO_01.json
-# CRIO_SOLUTION_AND_STUB_START_MODULE_TESTNG
-pip3 install -r assesment/xmlAssessment/requirements.txt
-python3 assesment/xmlAssessment/xmlAssessment.py assesment/xmlAssessment/Assessment_Instruction.json test-results.xml
-# CRIO_SOLUTION_AND_STUB_END_MODULE_TESTNG
+
+
+
+
