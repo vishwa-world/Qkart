c7fedc99f47ad840bf8420cc76ecac5aeb50e2e6
Nabhan <nabhan@criodo.com>
2022-06-28 14:33:56 +0550

    updated changes - 2022-06-28 14:13:56

--- run_Platform_Assesment.sh
+++ run_Platform_Assesment.sh
@@ -1,1 +1,4 @@
-python3 assesment/remoteAssesment.py "0.0.0.0" assesment/AIS_MO_01.json
+# python3 assesment/remoteAssesment.py "0.0.0.0" assesment/AIS_MO_01.json
+ cp /tmp/external_build/test-results/test/TEST-QKART_TESTNG.QKART_Tests.xml test-results.xml
+pip3 install -r assesment/xmlAssessment/requirements.txt
+python3 assesment/xmlAssessment/xmlAssessment.py assesment/xmlAssessment/Assessment_Instruction.json /tmp/external_build/test-results/test/TEST-QKART_TESTNG.QKART_Tests.xml
