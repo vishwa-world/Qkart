3bde8206430438abc0e95f27e6e5de38a8e49ece
Nabhan <nabhan@criodo.com>
2022-07-01 17:50:58 +0550

    updated changes - 2022-07-01 17:30:58

--- run_Platform_Assesment.sh
+++ run_Platform_Assesment.sh
@@ -1,3 +1,3 @@
-cp /tmp/external_build/test-results/test/TEST-QKART_TESTNG.QKART_Tests.xml test-results.xml
+cp /tmp/external_build/reports/tests/test/testng-results.xml test-results.xml
 pip3 install -r assesment/xmlAssessment/requirements.txt
-python3 assesment/xmlAssessment/xmlAssessment.py assesment/xmlAssessment/Assessment_Instruction.jso
+python3 assesment/xmlAssessment/xmlAssessment.py assesment/xmlAssessment/Assessment_Instruction.json test-results.xml
